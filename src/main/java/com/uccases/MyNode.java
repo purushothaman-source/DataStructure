23e1c3fbfb7e8fee3e095b527bf985ac7a6ce79b
purushothaman-source <purushoth99jun@gmail.com>
2021-01-26 20:37:39 +0530

    ADDED : functionaity to test simple LinkedList

--- src/main/java/com/uccases/MyNode.java
+++ src/main/java/com/uccases/MyNode.java
@@ -1,4 +1,27 @@
 package com.uccases;
 
-public class MyNode<T> {
+public class MyNode<K> {
+    private  MyNode next;
+    private K key;
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
 }
--- src/test/java/com/uccases/MyNodeTest.java
+++ src/test/java/com/uccases/MyNodeTest.java
@@ -1,10 +1,18 @@
 package com.uccases;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MyNodeTest {
     @Test
     public void given3Number_WhenLinkedShouldPassed_LinkedListTest() {
-        new MyNode<Integer>();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> mythirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(mythirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                myFirstNode.getNext().getNext().equals(mythirdNode);
+        Assert.assertTrue(result);
     }
 }
